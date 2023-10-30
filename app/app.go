d3a90bda093bd0883e7f15ade1c67c3227ac2fc5
iiacoban42 <i.p.iacoban@gmail.com>
2023-10-30 22:42:00 +0100

    Init FL app channel

---
+++ app/app.go
@@ -0,0 +1,225 @@
+// Copyright 2021 PolyCrypt GmbH, Germany
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package app
+
+import (
+	"fmt"
+	"io"
+	"log"
+
+	"github.com/pkg/errors"
+
+	"perun.network/go-perun/channel"
+	"perun.network/go-perun/wallet"
+)
+
+// FLApp is a channel app.
+type FLApp struct {
+	Addr wallet.Address
+}
+
+func NewFLApp(addr wallet.Address) *FLApp {
+	return &FLApp{
+		Addr: addr,
+	}
+}
+
+// Def returns the app address.
+func (a *FLApp) Def() wallet.Address {
+	return a.Addr
+}
+
+func (a *FLApp) InitData(firstActor channel.Index) *FLAppData {
+	return &FLAppData{
+		NextActor: uint8(firstActor),
+	}
+}
+
+// DecodeData decodes the channel data.
+func (a *FLApp) DecodeData(r io.Reader) (channel.Data, error) {
+	d := FLAppData{}
+
+	var err error
+	d.NextActor, err = readUInt8(r)
+	if err != nil {
+		return nil, errors.WithMessage(err, "reading actor")
+	}
+
+	d.Model, err = readUInt8(r)
+	if err != nil {
+		return nil, errors.WithMessage(err, "reading model")
+	}
+
+	d.Weight, err = readUInt8(r)
+	if err != nil {
+		return nil, errors.WithMessage(err, "reading weight")
+	}
+
+	d.Accuracy, err = readUInt8(r)
+	if err != nil {
+		return nil, errors.WithMessage(err, "reading accuracy")
+	}
+
+	d.Loss, err = readUInt8(r)
+	if err != nil {
+		return nil, errors.WithMessage(err, "reading loss")
+	}
+
+	// grid, err := readUInt8Array(r, len(d.Grid))
+	// if err != nil {
+	// 	return nil, errors.WithMessage(err, "reading grid")
+	// }
+	// copy(d.Grid[:], makeFieldValueArray(grid))
+	return &d, nil
+}
+
+// ValidInit checks that the initial state is valid.
+func (a *FLApp) ValidInit(p *channel.Params, s *channel.State) error {
+	if len(p.Parts) != numParts {
+		return fmt.Errorf("invalid number of participants: expected %d, got %d", numParts, len(p.Parts))
+	}
+
+	appData, ok := s.Data.(*FLAppData)
+	if !ok {
+		return fmt.Errorf("invalid data type: %T", s.Data)
+	}
+
+	zero := FLAppData{}
+	// if appData.Grid != zero.Grid {
+	// 	return fmt.Errorf("invalid starting grid: %v", appData.Grid)
+	// }
+
+	if appData.Model != zero.Model {
+		return fmt.Errorf("invalid starting model: %v", appData.Model)
+	}
+
+	if appData.Weight != zero.Weight {
+		return fmt.Errorf("invalid starting weight: %v", appData.Weight)
+	}
+
+	if appData.Accuracy != zero.Accuracy {
+		return fmt.Errorf("invalid starting accuracy: %v", appData.Accuracy)
+	}
+
+	if appData.Loss != zero.Loss {
+		return fmt.Errorf("invalid starting loss: %v", appData.Loss)
+	}
+
+	if s.IsFinal {
+		return fmt.Errorf("must not be final")
+	}
+
+	if appData.NextActor >= numParts {
+		return fmt.Errorf("invalid next actor: got %d, expected < %d", appData.NextActor, numParts)
+	}
+	return nil
+}
+
+// ValidTransition is called whenever the channel state transitions.
+func (a *FLApp) ValidTransition(params *channel.Params, from, to *channel.State, idx channel.Index) error {
+	err := channel.AssetsAssertEqual(from.Assets, to.Assets)
+	if err != nil {
+		return fmt.Errorf("Invalid assets: %v", err)
+	}
+
+	fromData, ok := from.Data.(*FLAppData)
+	if !ok {
+		panic(fmt.Sprintf("from state: invalid data type: %T", from.Data))
+	}
+
+	toData, ok := to.Data.(*FLAppData)
+	if !ok {
+		panic(fmt.Sprintf("to state: invalid data type: %T", from.Data))
+	}
+
+	// Check actor.
+	if fromData.NextActor != uint8safe(uint16(idx)) {
+		return fmt.Errorf("invalid actor: expected %v, got %v", fromData.NextActor, idx)
+	}
+
+	// Check next actor.
+	if len(params.Parts) != numParts {
+		panic("invalid number of participants")
+	}
+	expectedToNextActor := calcNextActor(fromData.NextActor)
+	if toData.NextActor != expectedToNextActor {
+		return fmt.Errorf("invalid next actor: expected %v, got %v", expectedToNextActor, toData.NextActor)
+	}
+
+	// Check grid.
+	// changed := false
+	// for i, v := range toData.Grid {
+	// 	if v > maxFieldValue {
+	// 		return fmt.Errorf("invalid grid value at index %d: %d", i, v)
+	// 	}
+	// 	vFrom := fromData.Grid[i]
+	// 	if v != vFrom {
+	// 		if vFrom != notSet {
+	// 			return fmt.Errorf("cannot overwrite field %d", i)
+	// 		}
+	// 		if changed {
+	// 			return fmt.Errorf("cannot change two fields")
+	// 		}
+	// 		changed = true
+	// 	}
+	// }
+	//
+	// if !changed {
+	// 	return fmt.Errorf("cannot skip turn")
+	// }
+
+	// check round inputs
+
+
+
+		// Check model.
+	// if toData.Model > maxFieldValue {
+	// 		return fmt.Errorf("invalid model value: %d", toData.Model)
+	// 	}
+
+
+	// Check final and allocation.
+	isFinal, winner := toData.CheckFinal()
+	if to.IsFinal != isFinal {
+		return fmt.Errorf("final flag: expected %v, got %v", isFinal, to.IsFinal)
+	}
+	expectedAllocation := from.Allocation.Clone()
+	if winner != nil {
+		expectedAllocation.Balances = computeFinalBalances(from.Allocation.Balances, *winner)
+	}
+	if err := expectedAllocation.Equal(&to.Allocation); err != nil {
+		return errors.WithMessagef(err, "wrong allocation: expected %v, got %v", expectedAllocation, to.Allocation)
+	}
+	return nil
+}
+
+
+func (a *FLApp) Set(s *channel.State, model, weight, accuracy, loss int, actorIdx channel.Index) error {
+	d, ok := s.Data.(*FLAppData)
+	if !ok {
+		return fmt.Errorf("invalid data type: %T", d)
+	}
+
+	d.Set(model, weight, accuracy, loss, actorIdx)
+	log.Println("\n" + d.String())
+
+	if isFinal, winner := d.CheckFinal(); isFinal {
+		s.IsFinal = true
+		if winner != nil {
+			s.Balances = computeFinalBalances(s.Balances, *winner)
+		}
+	}
+	return nil
+}
---
+++ app/data.go
@@ -0,0 +1,93 @@
+package app
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+
+	"github.com/pkg/errors"
+	"perun.network/go-perun/channel"
+)
+
+// FLAppData is the app data struct.
+// Grid:
+// 0 1 2
+// 3 4 5
+// 6 7 8
+type FLAppData struct {
+	NextActor uint8
+	// Grid      [9]FieldValue
+	Model 	  uint8
+	Weight 	  uint8
+	Accuracy  uint8
+	Loss 	  uint8
+}
+
+func (d *FLAppData) String() string {
+	var b bytes.Buffer
+	fmt.Fprintf(&b, "model: %v\n", d.Model)
+	fmt.Fprintf(&b, "weight: %v\n", d.Weight)
+	fmt.Fprintf(&b, "accuracy: %v\n", d.Accuracy)
+	fmt.Fprintf(&b, "loss: %v\n", d.Loss)
+	fmt.Fprintf(&b, "Next actor: %v\n", d.NextActor)
+	return b.String()
+}
+
+// Encode encodes app data onto an io.Writer.
+func (d *FLAppData) Encode(w io.Writer) error {
+	err := writeUInt8(w, d.NextActor)
+	if err != nil {
+		return errors.WithMessage(err, "writing actor")
+	}
+
+	err = writeUInt8(w, d.Model)
+	if err != nil {
+		return errors.WithMessage(err, "writing model")
+	}
+
+	err = writeUInt8(w, d.Weight)
+	if err != nil {
+		return errors.WithMessage(err, "writing weight")
+	}
+
+	err = writeUInt8(w, d.Accuracy)
+	if err != nil {
+		return errors.WithMessage(err, "writing accuracy")
+	}
+
+	err = writeUInt8(w, d.Loss)
+	return errors.WithMessage(err, "writing loss")
+
+	// err = writeUInt8Array(w, makeUInt8Array(d.Grid[:]))
+	// return errors.WithMessage(err, "writing grid")
+
+}
+
+// Clone returns a deep copy of the app data.
+func (d *FLAppData) Clone() channel.Data {
+	_d := *d
+	return &_d
+}
+
+func (d *FLAppData) Set(model, weight, accuracy, loss int, actorIdx channel.Index) {
+	if d.NextActor != uint8safe(uint16(actorIdx)) {
+		panic("invalid actor")
+	}
+	// v := makeFieldValueFromPlayerIdx(actorIdx)
+	// d.Grid[y*3+x] = v
+
+	if d.NextActor == 0 {
+		d.Model = uint8(model)
+		d.Accuracy = uint8(accuracy)
+		d.Loss = uint8(loss)
+
+	} else {
+		d.Weight = uint8(weight)
+	}
+
+	d.NextActor = calcNextActor(d.NextActor)
+}
+
+func calcNextActor(actor uint8) uint8 {
+	return (actor + 1) % numParts
+}
